9a076bad770f7f7ff6c7476e8b407d62bc705345
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2017-12-25 08:55:42 +0200

    "Redirected to production DB"

--- data/data.js
+++ data/data.js
@@ -2,4 +2,4 @@
 var config = require('../env/config')[env];*/
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/suggestionBoxDB",{useMongoClient:true}); 
+mongoose.connect("mongodb://zarak995:ZaraKhumba23@cluster0-shard-00-00-wxeue.mongodb.net:27017,cluster0-shard-00-01-wxeue.mongodb.net:27017,cluster0-shard-00-02-wxeue.mongodb.net:27017/suggestionBoxDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",{useMongoClient:true});
