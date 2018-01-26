8903af18412ad18ff75c68199d80254fc5bc5de0
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-01-27 01:11:25 +0200

    "Addedd getCurrent avatar endpoint"

--- api/controllers/avatarController.js
+++ api/controllers/avatarController.js
@@ -3,14 +3,24 @@ var Avatar = mongoose.model('Avatars');
 var User = mongoose.model('Users');
 
 exports.list_user_avatars = function (req, res) {
-    Avatar.find({ user: req.params.userId }, function (err, avatar) {
+    Avatar.find({ user: req.params.userId }, function (err, list_avatar) {
         if (err)
             res.send(err)
-        res.json(avatar);
+        res.json(list_avatar);
     })
 }
 
+exports.get_current_avatar = function (req, res) {
+    Avatar.find({ user: req.params.userId }).sort({ _id: -1 }).limit(1)
+        .exec(function (err, avatar) {
+            if (err)
+                res.send(err)
+            res.json(avatar);
+        })
+}
+
 exports.view_an_avatar = function (req, res) {
+
     Avatar.findById({
         _id: req.param.avatarId
     }, function (err, avatar) {
@@ -21,6 +31,7 @@ exports.view_an_avatar = function (req, res) {
 }
 
 exports.create_an_avatar = function (req, res) {
+    console.log("here");
     var new_avatar = new Avatar(req.body);
     new_avatar.save(function (err, avatar) {
         if (err)
--- api/routes/avatarRoutes.js
+++ api/routes/avatarRoutes.js
@@ -2,6 +2,6 @@
 module.exports = function (app, data) {
     var avatar = require('../controllers/avatarController');
     app.route('/avatars/:userId')
-        .get(avatar.list_user_avatars)
+        .get(avatar.get_current_avatar)
         .post(avatar.create_an_avatar);
 }
