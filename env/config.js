c47aaf435dc3bd7f6cfc14f8a7842b115099aa4f
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-02 13:46:08 +0200

    "Develpment and prod environments uses the same key"

--- env/config.js
+++ env/config.js
@@ -1,8 +1,8 @@
 var config = {
     development: {
         //Development database url
-        dburl: 'mongodb://localhost/suggestionBoxDB',
-        secret: 'kjsdhfjskdhfjskhfuefhksdjfhksdjfkjsdhfjskdhfjskhfue2122232425262728292010fhks2122232425262728292010djfh2122232425262728292010k2122232425262728292010sdjf',
+        dburl: 'mongodb://zarak995:ZaraKhumba23@cluster0-shard-00-00-wxeue.mongodb.net:27017,cluster0-shard-00-01-wxeue.mongodb.net:27017,cluster0-shard-00-02-wxeue.mongodb.net:27017/suggestionBoxDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+        secret: '123Secret?456?anothersecret??**how_Have_=_uBeen_BoBBo..<<kjsdh5262728292010k21sdfsdf2s22324252627sdfsdfsdf28292010sdjf>>:""QWE nm.,nmdgdfgdfgd',
         //server details
         server: {
             host: '127.0.0.1',
