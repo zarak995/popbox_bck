99eb12e30a3ba17e5b5f997e6a5ce509365d5cc5
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-04 01:24:42 +0200

    "Added user chats route"

--- api/routes/chatRoutes.js
+++ api/routes/chatRoutes.js
@@ -10,11 +10,16 @@ module.exports = function (app, data) {
         .get(chat.list_all_chats)
         .post(chat.create_a_chat);
 
+    app.route('/chats/top/chats')
+        .get(chat.list_top_chats);
+
+    app.route('/chats/users/:userId')
+        .get(chat.user_chats);
+
     app.route('/chats/:chatId')
         .get(chat.view_a_chat)
         .put(chat.update_a_chat)
         .delete(chat.delete_a_chat);
 
-    app.route('/chats/top/chats')
-        .get(chat.list_top_chats);
+
 }
