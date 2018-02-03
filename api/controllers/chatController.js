0a6b935d02a3f927e903ad4548e8c61baa6ddb23
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-04 01:24:11 +0200

    "Returning one user chats"

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -2,6 +2,17 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var Chat = mongoose.model('Chats');
 
+exports.list_top_chats = function (req, res) {
+    Chat.find().sort({ post: -1 }).limit(15)
+        .populate({ path: 'likes', model: 'Avatars' })
+        .populate({ path: 'owner', model: 'Avatars' })
+        .exec((err, chat) => {
+            if (err) res.send(err);
+            else {
+                res.json(chat)
+            }
+        })
+}
 exports.list_all_chats = function (req, res) {
     Chat.find().sort({ _id: -1 })
         .populate({ path: 'likes', model: 'Avatars' })
@@ -29,7 +40,6 @@ exports.view_a_chat = function (req, res) {
         .exec((err, chat) => {
             if (err) res.send(err);
             else {
-                console.log(chat)
                 res.json(chat)
             }
         })
@@ -45,7 +55,6 @@ exports.create_a_chat = function (req, res) {
         }
     })
 }
-
 exports.update_a_chat = function (req, res) {
     Chat.findByIdAndUpdate(req.params.chatId, {
         $set: {
@@ -82,15 +91,24 @@ exports.delete_a_chat = function (req, res) {
             }
         })
 }
-
-exports.list_top_chats = function (req, res) {
-    Chat.find().sort({ post: -1 }).limit(15)
+exports.user_chats = function (req, res) {
+    Chat.find({})
         .populate({ path: 'likes', model: 'Avatars' })
         .populate({ path: 'owner', model: 'Avatars' })
-        .exec((err, chat) => {
-            if (err) res.send(err);
+        .populate({ path: 'reports', model: 'Avatars' })
+        .exec((err, list) => {
+            if (err) {
+                res.send("There was a problem, please try again later");
+            }
             else {
-                res.json(chat)
+                let maxLength = list.length;
+                let responseList = [];
+                for (var x = 0; x < maxLength; x++) {
+                    if (list[x].owner.user == req.params.userId) {
+                        responseList.push(list[x]);
+                    }
+                }
+                res.json(responseList);
             }
         })
 }
