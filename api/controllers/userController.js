088cebce56443c80d6c139c883fb4bf9036bd3b1
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-05 04:51:30 +0200

    "Change password, update user"

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,32 +1,49 @@
 var mongoose = require('mongoose'),
     User = mongoose.model('Users');
 
-exports.list_all_users = function(req,res){
-    User.find({},function(err,user){
-        if(err)
-            res.send(err)
-        res.json(user);
-    })
-}
-exports.view_a_user = function(req,res){
-    User.findById({_id:req.params.userId},function(err,user){
-        if(err)
+exports.list_all_users = function (req, res) {
+    User.find({}, function (err, user) {
+        if (err)
             res.send(err)
         res.json(user);
     })
 }
 
-exports.update_a_user = function(req,res){
-    User.findOneAndUpdate({_id: req.params.userId},req.body,{new:true},function(err,user){
-        if(err)
+exports.view_a_user = function (req, res) {
+    User.findById({ _id: req.params.userId }, function (err, user) {
+        if (err)
             res.send(err)
         res.json(user);
     })
 }
-exports.delete_a_user = function(req,res){
-    User.remove({_id: req.params.userId},function(err,user){
-        if(err)
-            res.send(err)
-        res.json({message: 'User deleted successfully.'});
+
+exports.update_a_user = function (req, res) {
+    console.log("its here");
+   User.findByIdAndUpdate(req.params.userId, {
+        $set: {
+            phone: req.body.phone,
+            email: req.body.email
+        }
     })
+        .exec((err, user) => {
+            console.log(user)
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                console.log(user);                
+                res.json(user);
+            }
+        })
+}
+exports.delete_a_user = function (req, res) {
+    User.remove({ _id: req.params.userId }, function (err, user) {
+        if (err)
+            res.send(err)
+        res.json({ message: 'User deleted successfully.' });
+    })
+}
+
+exports.change_user_password = function () {
+
 }
