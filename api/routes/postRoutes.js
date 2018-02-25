d588e47a805839ccf95420bef7b0893614676677
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-25 03:24:08 +0200

    "changes on postRoutes"

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -6,14 +6,14 @@ module.exports = function(app, data){
     console.log("New Post POST /posts")
     console.log("View Post GET /posts/:postId")
     console.log("Update Post PUT /posts/:postId")
-    console.log("Delete Post DELETE /posts/:postId")
+    console.log("Delete Post DELETE /posts/:chatId/:postId")
     
     
     app.route('/posts')
         .get(post.list_all_posts)
         .post(post.create_new_post);
 
-    app.route('/posts/:postId')
+    app.route('/posts/:chatId/:postId')
         .get(post.view_a_post)
         .put(post.update_a_post)
 
