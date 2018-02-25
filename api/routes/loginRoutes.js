df414174908d83ab9ff4ff5819c09f2b6c98a9b8
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-25 05:45:49 +0200

    "removed register routes"

--- api/routes/loginRoutes.js
+++ api/routes/loginRoutes.js
@@ -12,10 +12,7 @@ module.exports = function (app) {
     
     app.route('/login')
         .post(login.authenticate);
-
-    app.route('/login/reg')
-        .post(login.create_a_user);
-
+        
     app.route('/login/ver')
         .post(login.verify_a_user);
 
