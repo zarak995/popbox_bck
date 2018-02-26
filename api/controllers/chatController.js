1478c078704398b65e5ecad2ecc3bd1893defc8c
Njabulo Thwala <njabulo.thwala@symbiotics.co.za>
2018-02-27 00:24:30 +0200

    "Changes on Chat controller"

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -75,32 +75,62 @@ exports.create_a_chat = function (req, res) {
     })
 }
 exports.update_a_chat = function (req, res) {
-    Chat.findByIdAndUpdate(req.params.chatId, {
-        $set: {
-            body: req.body.body,
-            title: req.body.title,
-            likes: req.body.likes,
-            reports: req.body.reports
+    if (req.body == null) {
+        console.log('req body is null or undefined');
+        res.json(error)
+    } else {
+        if (req.body.body == null
+            || req.body.body == '') {
+            console.log('req chat.body is null or undefined');
+            res.json(error)
+        } else {
+            if (req.params.chatId == null
+                || req.params.chatId == '') {
+                console.log('req chat._id is null or undefined');
+                res.json(error);
+            } else {
+                if (req.body.likes == null
+                    || req.body.likes == ['']) {
+                    console.log('req chat.body is null or undefined');
+                    res.json(error)
+                } else {
+                    if (req.params.chatId == null
+                        || req.params.chatId == '') {
+                        res.json(error);
+                    } else {
+                        Chat.findByIdAndUpdate(req.params.chatId, {
+                            $set: {
+                                body: req.body.body,
+                                title: req.body.title,
+                                likes: req.body.likes,
+                                reports: req.body.reports
+                            }
+                        })
+                            .populate({ path: 'owner', model: 'Avatars' })
+                            .populate({
+                                path: 'post', model: 'Posts',
+                                populate: {
+                                    path: 'avatar',
+                                    model: 'Avatars'
+                                }
+                            })
+                            .exec((err, chat) => {
+                                if (err) {
+                                    console.log(err)
+                                    res.json(error)
+                                }
+                                else {
+                                    req.params.chatId = chat._id;
+                                    module.exports.view_a_chat(req, res)
+                                }
+                            })
+                    }
+                }
+            }
         }
-    })
-        .populate({ path: 'owner', model: 'Avatars' })
-        .populate({
-            path: 'post', model: 'Posts',
-            populate: {
-                path: 'avatar',
-                model: 'Avatars'
-            }
-        })
-        .exec((err, chat) => {
-            if (err) {
-                console.log(err)
-                res.json(error)
-            }
-            else {
-                req.params.chatId = chat._id;
-                module.exports.view_a_chat(req, res)
-            }
-        })
+    }
+
+
 }
 exports.delete_a_chat = function (req, res) {
     Chat.findByIdAndRemove({ _id: req.params.chatId })
